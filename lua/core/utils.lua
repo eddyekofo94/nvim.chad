c8244d1d887dd21d04c3edd72c90ccd3e4006728
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-12-13 09:40:09 +0000

    Initial commit

---
+++ lua/core/utils.lua
@@ -0,0 +1,193 @@
+local M = {}
+local merge_tb = vim.tbl_deep_extend
+
+M.load_config = function()
+  local config = require "core.default_config"
+  local chadrc_path = vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1]
+
+  if chadrc_path then
+    local chadrc = dofile(chadrc_path)
+
+    config.mappings = M.remove_disabled_keys(chadrc.mappings, config.mappings)
+    config = merge_tb("force", config, chadrc)
+    config.mappings.disabled = nil
+  end
+
+  return config
+end
+
+M.remove_disabled_keys = function(chadrc_mappings, default_mappings)
+  if not chadrc_mappings then
+    return default_mappings
+  end
+
+  -- store keys in a array with true value to compare
+  local keys_to_disable = {}
+  for _, mappings in pairs(chadrc_mappings) do
+    for mode, section_keys in pairs(mappings) do
+      if not keys_to_disable[mode] then
+        keys_to_disable[mode] = {}
+      end
+      section_keys = (type(section_keys) == "table" and section_keys) or {}
+      for k, _ in pairs(section_keys) do
+        keys_to_disable[mode][k] = true
+      end
+    end
+  end
+
+  -- make a copy as we need to modify default_mappings
+  for section_name, section_mappings in pairs(default_mappings) do
+    for mode, mode_mappings in pairs(section_mappings) do
+      mode_mappings = (type(mode_mappings) == "table" and mode_mappings) or {}
+      for k, _ in pairs(mode_mappings) do
+        -- if key if found then remove from default_mappings
+        if keys_to_disable[mode] and keys_to_disable[mode][k] then
+          default_mappings[section_name][mode][k] = nil
+        end
+      end
+    end
+  end
+
+  return default_mappings
+end
+
+M.load_mappings = function(section, mapping_opt)
+  vim.schedule(function()
+    local function set_section_map(section_values)
+      if section_values.plugin then
+        return
+      end
+
+      section_values.plugin = nil
+
+      for mode, mode_values in pairs(section_values) do
+        local default_opts = merge_tb("force", { mode = mode }, mapping_opt or {})
+        for keybind, mapping_info in pairs(mode_values) do
+          -- merge default + user opts
+          local opts = merge_tb("force", default_opts, mapping_info.opts or {})
+
+          mapping_info.opts, opts.mode = nil, nil
+          opts.desc = mapping_info[2]
+
+          vim.keymap.set(mode, keybind, mapping_info[1], opts)
+        end
+      end
+    end
+
+    local mappings = require("core.utils").load_config().mappings
+
+    if type(section) == "string" then
+      mappings[section]["plugin"] = nil
+      mappings = { mappings[section] }
+    end
+
+    for _, sect in pairs(mappings) do
+      set_section_map(sect)
+    end
+  end)
+end
+
+M.lazy_load = function(plugin)
+  vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile" }, {
+    group = vim.api.nvim_create_augroup("BeLazyOnFileOpen" .. plugin, {}),
+    callback = function()
+      local file = vim.fn.expand "%"
+      local condition = file ~= "NvimTree_1" and file ~= "[lazy]" and file ~= ""
+
+      if condition then
+        vim.api.nvim_del_augroup_by_name("BeLazyOnFileOpen" .. plugin)
+
+        -- dont defer for treesitter as it will show slow highlighting
+        -- This deferring only happens only when we do "nvim filename"
+        if plugin ~= "nvim-treesitter" then
+          vim.schedule(function()
+            require("lazy").load { plugins = plugin }
+
+            if plugin == "nvim-lspconfig" then
+              vim.cmd "silent! do FileType"
+            end
+          end, 0)
+        else
+          require("lazy").load { plugins = plugin }
+        end
+      end
+    end,
+  })
+end
+
+M.root_patterns = { ".git"}
+
+function M.get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace
+          and vim.tbl_map(function(ws)
+            return vim.uri_to_fname(ws.uri)
+          end, workspace)
+        or client.config.root_dir and { client.config.root_dir }
+        or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    ---@type string?
+    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  ---@cast root string
+  return root
+end
+-- this will return a function that calls telescope.
+-- cwd will default to utils.get_root
+-- for `files`, git_files or find_files will be chosen depending on .git
+function M.telescope(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
+M.get_relative_fname = function()
+  local fname = vim.fn.expand("%:p")
+  return fname:gsub(vim.fn.getcwd() .. "/", "")
+end
+
+M.get_relative_gitpath = function()
+  local fpath = vim.fn.expand("%:h")
+  local fname = vim.fn.expand("%:t")
+  local gitpath = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  local relative_gitpath = fpath:gsub(gitpath, "") .. "/" .. fname
+
+  return relative_gitpath
+end
+
+return M
